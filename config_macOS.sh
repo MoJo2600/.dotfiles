7bdc4a2abad425b4fe2dd084a6e6e9b8c1c3902c
christofwe <christof.weigl@gmail.com>
2019-01-01 12:33:13 +0100

    add energy settings and keyboard shortcuts

--- config_macOS.sh
+++ config_macOS.sh
@@ -3,24 +3,20 @@
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
-
 # Ask for the administrator password upfront
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
 
-# Set computer name (as done via System Preferences → Sharing)
+### Set computer name (as done via System Preferences → Sharing)
 #sudo scutil --set ComputerName "0x6D746873"
 #sudo scutil --set HostName "0x6D746873"
 #sudo scutil --set LocalHostName "0x6D746873"
 #sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6D746873"
 
 # check https://github.com/mathiasbynens/dotfiles/blob/master/.macos for more settings
+
 
 ### Dock 
 defaults write com.apple.dock static-only -bool TRUE
@@ -52,15 +48,23 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 /usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:labelOnBottom false" ~/Library/Preferences/com.apple.finder.plist
 #killall Finder
 
+
 ### System
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
 # Show the /Volumes folder
 sudo chflags nohidden /Volumes
+
+
+### Power
+sudo pmset -b displaysleep 10
+sudo pmset -c displaysleep 60
+
 
 # Get colortheme for iTerm
 curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Monokai%20Remastered.itermcolors -o Monokai\ Remastered.itermcolors
