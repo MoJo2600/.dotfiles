c34ce3c56209421a0ea8a1a6eaf5ff8bff58c26f
christofwe <christof.weigl@gmail.com>
2019-01-06 12:03:14 +0100

    alias for proxy

--- work.zsh
+++ work.zsh
@@ -5,4 +5,9 @@ then
     rm ~/.aws/current_profile &> /dev/null
 fi
 
+PROXY_URL="http://cias.geoaws.com:8080/"
+export http_proxy=${PROXY_URL}
+export https_proxy=${PROXY_URL}
+export no_proxy="localhost, 127.0.0.1,.conti.de,.contiwan.com"
+
 PROMPT="$(cat ~/.aws/current_profile 2> /dev/null)${PROMPT}"
